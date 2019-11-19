96b52be93c81cbfee1628220fd04017baf182fb1
Ruben Zagon <ruben.zagon@gmail.com>
2019-11-19 19:20:14 +0000

    Fixit a error with the directory where save de keys

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ echo "Importing  SSH Key."
 echo "REMOTE_USER = ${REMOTE_USER}"
 echo "REMOTE_HOST = ${REMOTE_HOST}"
 
-openssl aes-256-cbc -K $encrypted_ff401662a16b_key -iv $encrypted_ff401662a16b_iv -in deploy-travis.enc -out ./keys/deploy-travis -d
+openssl aes-256-cbc -K $encrypted_ff401662a16b_key -iv $encrypted_ff401662a16b_iv -in deploy-travis.enc -out deploy-travis -d
 
 eval "$(ssh-agent -s)"
 
